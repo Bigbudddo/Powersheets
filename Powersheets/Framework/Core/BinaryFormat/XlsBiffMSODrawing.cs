81ccc50711eb2798ebf622f9284f25a368afd1aa
Stuart Harrison <email@stuart-harrison.com>
2017-04-12 10:46:30 +0100

    Initial Framework Build Push
    Base code/functions from ExcelDataReader has been replicated into the Framework folder, eliminating the need to add an additional reference to the ExcelDataReader to get code to run.

---
+++ Powersheets/Framework/Core/BinaryFormat/XlsBiffMSODrawing.cs
@@ -0,0 +1,15 @@
+using Excel;
+
+namespace ExcelDataReader.Portable.Core.BinaryFormat
+{
+	/// <summary>
+	/// Represents MSO Drawing record
+	/// </summary>
+	internal class XlsBiffMSODrawing : XlsBiffRecord
+	{
+        internal XlsBiffMSODrawing(byte[] bytes, uint offset, ExcelBinaryReader reader)
+			: base(bytes, offset, reader)
+		{
+		}
+	}
+}
